7e6c16de3edf6aeb861c6454b9f84a642c67b9de
Loan <loan181@hotmail.fr>
2018-06-29 17:10:03 +0200

    Best management of mame register frame

--- src/main.lua
+++ src/main.lua
@@ -16,6 +16,7 @@ require("creature")
 require("neuron")
 require("mapView")
 require("generation")
+require("mameController")
 
 
 
@@ -50,56 +51,19 @@ local map = MapView(6, 6, 13, MameCst.screen, 4, 4, 4)
 
 local inputsManager = Inputs(MameCst.screen, 120, 4, 4, 2, MameCst.ioP1)
 
-local neuron = Neuron(map, 10, 12, tileEnum.solidTile, inputsManager, 1, MameCst.ioP1)
+--local neuron = Neuron(map, 10, 12, tileEnum.solidTile, inputsManager, 1, MameCst.ioP1)
 --local neuron2 = Neuron(map, 10, 8, tileEnum.solidTile, inputsManager, 8, MameCst.ioP1)
 --local neuron3 = Neuron(map, 8, 10, tileEnum.solidTile, inputsManager, 8, MameCst.ioP1)
 --local neuron4 = Neuron(map, 10, 12, tileEnum.freeTile, inputsManager, 8, MameCst.ioP1)
 --local neuron5 = Neuron(map, 9, 11, tileEnum.enemy, inputsManager, 8, MameCst.ioP1)
---local creature = Creature(map, inputsManager, {neuron}, MameCst.screen, MameCst.ioP1, 160, 4)
+--local creature = Creature(map, inputsManager, {neuron, neuron2, neuron3, neuron4}, MameCst.screen, MameCst.ioP1, 160, 4)
 
 local gen = Generation(5, map, inputsManager)
 gen:randomizeAll()
 
-local creature = gen:getNextCreature()
-local lastFitness = 0
-local timer = 0
-local lastTimeOut = 0
-MameCst.machine:load("start")
-
-
-MameCst.emu.register_frame(
-		function()
-			timer = timer + 1
-			-- Go to next creature if dead, or timeout keeping the same fitness
-			local isTimeOut = timer - lastTimeOut >= 60*3 -- 8 seconds of timeout
-			local curFitness = creature:getFitness()
-			if (creature:isDead() or ( isTimeOut and lastFitness == curFitness)) then
-				gen:creatureIsDead()
-				MameCst.machine:load("start")
-				creature = gen:getNextCreature()
-				lastFitness = 0
-				timer = 0
-				lastTimeOut = 0
-			elseif (isTimeOut and lastFitness ~= curFitness) then
-				lastFitness = curFitness
-				lastTimeOut = timer
-			end
-
-			map:update()
-			creature:updateFitness()
-			creature:updateNeurons()
-		end
-)
-
-MameCst.emu.register_frame_done(
-    function()
-		map:draw()
-		inputsManager:draw()
-		if creature ~= nil then
-			creature:draw()
-		end
-	end
-)
+local mameController = MameController(map, inputsManager, gen, 3)
+mameController:registerFrame()
+mameController:registerFrameDone()
 
 
 
